9ba8a397815e4cc45eda1eedf3f485a0ffb45216
Rashidov01 <113158224+Rashidovpro@users.noreply.github.com>
2022-09-18 21:04:41 +0500

    front

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  images: { unoptimized: true },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
